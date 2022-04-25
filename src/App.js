227aea7515540b8a522f1e5b9de408e76247ee11
saidugurlu <ugurlumsaid@gmx.de>
2022-04-25 13:21:34 +0200

    edit switchCase (default added)

--- src/App.js
+++ src/App.js
@@ -1,83 +1,84 @@
-import { useState } from 'react';
-import './App.scss';
-import _jobs from './data/jobs.json';
+import { useState } from "react";
+import "./App.scss";
+import _jobs from "./data/jobs.json";
 
 _jobs.forEach((job) => {
-	job.status = 'send';
+  job.status = "send";
 });
 
 function App() {
-	const [displayKind, setDisplayKind] = useState('full');
-	const [jobs, setJobs] = useState(_jobs);
+  const [displayKind, setDisplayKind] = useState("full");
+  const [jobs, setJobs] = useState(_jobs);
 
-	const handleToggleView = () => {
-		setDisplayKind(displayKind === 'full' ? 'list' : 'full');
-	};
+  const handleToggleView = () => {
+    setDisplayKind(displayKind === "full" ? "list" : "full");
+  };
 
-	const handleStatusChange = (job) => {
-		switch (job.status) {
-			case 'send':
-				job.status = 'wait';
-				break;
-			case 'wait':
-				job.status = 'interview';
-				break;
-			case 'interview':
-				job.status = 'declined';
-				break;
-			case 'declined':
-				job.status = 'accepted';
-				break;
-			case 'accepted':
-				job.status = 'send';
-				break;
-		}
-		setJobs([...jobs]);
-	};
+  const handleStatusChange = (job) => {
+    switch (job.status) {
+      case "send":
+        job.status = "wait";
+        break;
+      case "wait":
+        job.status = "interview";
+        break;
+      case "interview":
+        job.status = "declined";
+        break;
+      case "declined":
+        job.status = "accepted";
+        break;
+      case "accepted":
+        job.status = "send";
+        break;
+      default:
+      // do nothing
+    }
 
-	return (
-		<div className="App">
-			<h1>Job Application Process</h1>
-			<button onClick={handleToggleView}>Toggle View</button>
-			{displayKind === 'full' ? (
-				<div className="jobs">
-					{jobs.map((job, index) => {
-						return (
-							<div key={index} className={`job ${job.status}`}>
-								<div className="header">
-									<div className="position">
-										<a href={job.url}>{job.position}</a>
-									</div>
-									<button
-										onClick={() => handleStatusChange(job)}
-										className="status"
-									>
-										{job.status}
-									</button>
-								</div>
-								<div className="skills">{job.skills}</div>
-								{job.status !== 'declined' && (
-									<div className="bulkText">
-										{job.bulkText}
-									</div>
-								)}
-							</div>
-						);
-					})}
-				</div>
-			) : (
-				<ul className="jobsList">
-					{jobs.map((job, index) => {
-						return (
-							<li key={index} className="job">
-								<a href={job.url}>{job.position}</a>
-							</li>
-						);
-					})}
-				</ul>
-			)}
-		</div>
-	);
+    setJobs([...jobs]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Job Application Process</h1>
+      <button onClick={handleToggleView}>Toggle View</button>
+      {displayKind === "full" ? (
+        <div className="jobs">
+          {jobs.map((job, index) => {
+            return (
+              <div key={index} className={`job ${job.status}`}>
+                <div className="header">
+                  <div className="position">
+                    <a href={job.url}>{job.position}</a>
+                  </div>
+                  <button
+                    onClick={() => handleStatusChange(job)}
+                    className="status"
+                  >
+                    {job.status}
+                  </button>
+                </div>
+                <div className="skills">{job.skills}</div>
+                {job.status !== "declined" && (
+                  <div className="bulkText">{job.bulkText}</div>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <ul className="jobsList">
+          {jobs.map((job, index) => {
+            return (
+              <li key={index} className="job">
+                <a href={job.url}>{job.position}</a>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default App;
