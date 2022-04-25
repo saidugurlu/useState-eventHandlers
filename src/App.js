94d9d45b5be01b71342f022c45dc839e14c7ce98
saidugurlu <ugurlumsaid@gmx.de>
2022-04-25 10:38:46 +0200

    data json added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.scss';
+import jobs from './data/jobs.json';
 
 function App() {
-  return (
-    <div className="App">
- hallo
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Job Application Process</h1>
+			<div className="jobs">
+				{jobs.map((job, index) => {
+					return <div className="job">nnn</div>;
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default App;
