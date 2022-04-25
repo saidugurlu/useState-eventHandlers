556d1cd43a180ee596f5d3762e7553550ef5063d
saidugurlu <ugurlumsaid@gmx.de>
2022-04-25 10:51:14 +0200

    job list created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './App.scss';
 import jobs from './data/jobs.json';
 
+const displayKind = 'list';
+
 function App() {
 	return (
 		<div className="App">
 			<h1>Job Application Process</h1>
-			<div className="jobs">
-				{jobs.map((job, index) => {
-					return <div className="job">nnn</div>;
-				})}
-			</div>
+			{displayKind === 'full' ? (
+				<div className="jobs">
+					{jobs.map((job, index) => {
+						return (
+							<div key={index} className="job">
+								<div className="position">
+									<a href={job.url}>{job.position}</a>
+								</div>
+								<div className="skills">{job.skills}</div>
+								<div className="bulkText">{job.bulkText}</div>
+							</div>
+						);
+					})}
+				</div>
+			) : (
+				<ul className="jobsList">
+					{jobs.map((job, index) => {
+						return (
+							<li key={index} className="job"><a href={job.url}>{job.position}</a></li>
+						);
+					})}
+				</ul>
+			)}
 		</div>
 	);
 }
