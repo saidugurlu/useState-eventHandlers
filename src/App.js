afb6fffc245d3164f9ff6b31629adbb330ae5818
saidugurlu <ugurlumsaid@gmx.de>
2022-04-25 11:11:03 +0200

    toggle added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import './App.scss';
 import jobs from './data/jobs.json';
 
-const displayKind = 'list';
-
 function App() {
+	const [displayKind, setDisplayKind] = useState('full');
+
+	const handleToggleView = () => {
+		setDisplayKind(displayKind === 'full' ? 'list' : 'full');
+	};
+
 	return (
 		<div className="App">
 			<h1>Job Application Process</h1>
+			<button onClick={handleToggleView}>Toggle View</button>
 			{displayKind === 'full' ? (
 				<div className="jobs">
 					{jobs.map((job, index) => {
@@ -25,7 +31,9 @@ function App() {
 				<ul className="jobsList">
 					{jobs.map((job, index) => {
 						return (
-							<li key={index} className="job"><a href={job.url}>{job.position}</a></li>
+							<li key={index} className="job">
+								<a href={job.url}>{job.position}</a>
+							</li>
 						);
 					})}
 				</ul>
